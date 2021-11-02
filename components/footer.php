08a70e954bc5e95ad34312600650db237a4f89e6
Murillo Silva <mfs.murillo@gmail.com>
2021-11-02 11:19:56 -0300

    fix: footer

--- components/footer.php
+++ components/footer.php
@@ -81,7 +81,7 @@
         <div class="row">
             <div class="col-12 col-md-6 col-lg-6">
                 <div class="left-side">
-                    <p>Copyright © 2021 Techcontrol. Todos os direitos reservados</p>
+                    <p>Copyright © 2021 B3 Marketing. Todos os direitos reservados</p>
                 </div>
             </div>
 
